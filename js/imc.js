799a3edfb6ade788314ec04aa718b24dcb31e0fd
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-03-17 16:51:50 -0300

    Análise de todos os pacientes

--- js/imc.js
+++ js/imc.js
@@ -1,27 +1,37 @@
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
+//coleta todos os pacientes em um array
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-var imc = peso / (altura * altura)
+console.log(pacientes)
 
-var pesoEValido = true;
-var alturaEValida = true;
+for (var i = 0; i < pacientes.length; i++)
+{
+    var paciente = pacientes[i];
 
-if (peso <= 0 || peso >= 1000){
-    console.log("Peso Inválido");
-    pesoEValido = false;
-    tdImc.textContent = "Peso Inválido";
-}
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
+    var tdImc = paciente.querySelector(".info-imc");
 
-if (altura <= 0 || altura >= 3){
-    console.log("Altura Inválida");
-    alturaEValida = false;
-    tdImc.textContent = "Altura Inválida"
-}
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    var imc = peso / (altura * altura); 
 
-if (pesoEValido && alturaEValida){
-    tdImc.textContent = imc;
+    var pesoEValido = true;
+    var alturaEValida = true;
+
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso Inválido");
+        pesoEValido = false;
+        tdImc.textContent = "Peso Inválido";
+    }
+
+    if (altura <= 0 || altura >= 3){
+        console.log("Altura Inválida");
+        alturaEValida = false;
+        tdImc.textContent = "Altura Inválida";
+    }
+
+    if (pesoEValido && alturaEValida){
+        tdImc.textContent = imc.toFixed(2);
+        //Limita a exibição a duas casas decimais
+    }
 }
