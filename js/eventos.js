50d4669598a93b002cff36121500368513293ed6
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-03-18 18:01:46 -0300

    Mudança dos eventos padrões

--- js/eventos.js
+++ js/eventos.js
@@ -1,15 +1,6 @@
-var titulo = document.querySelector(".title")
-
-titulo.addEventListener("click", function (){
-    alert("CLICK!");
-});
-
-function mostrarMensagem() {
-    alert("CLICK!");
-}
-
 var botao_adicionar = document.querySelector("#adicionar-paciente");
 
-botao_adicionar.addEventListener("click", function(){
-    
+botao_adicionar.addEventListener("click", function(event){
+    event.preventDefault();
+    alert("click")
 });
