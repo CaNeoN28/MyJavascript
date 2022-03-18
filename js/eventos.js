285b12c3d9bd49fc4def278e9b86d73128cf804a
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-03-18 17:56:22 -0300

    Eventos básicos

---
+++ js/eventos.js
@@ -0,0 +1,15 @@
+var titulo = document.querySelector(".title")
+
+titulo.addEventListener("click", function (){
+    alert("CLICK!");
+});
+
+function mostrarMensagem() {
+    alert("CLICK!");
+}
+
+var botao_adicionar = document.querySelector("#adicionar-paciente");
+
+botao_adicionar.addEventListener("click", function(){
+    
+});
