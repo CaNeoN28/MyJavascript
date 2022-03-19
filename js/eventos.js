50107013a284c20a2f2255bd4e6061810e2c10d7
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-03-19 16:36:32 -0300

    Adição de pacientes

--- js/eventos.js
+++ js/eventos.js
@@ -1,6 +1,34 @@
 var botao_adicionar = document.querySelector("#adicionar-paciente");
 
 botao_adicionar.addEventListener("click", function(event){
+
     event.preventDefault();
-    alert("click")
+
+    var form = document.querySelector('#form-adiciona');
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 });
